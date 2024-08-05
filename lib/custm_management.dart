6a7fba53756e882e0c376409e77838173aab6839
SOUMYA RANJAN BARIK <123263359+sr-barik@users.noreply.github.com>
2024-08-05 15:31:03 +0530

    updated

--- lib/custm_management.dart
+++ lib/custm_management.dart
@@ -8,24 +8,43 @@ class ManagementSystem extends StatefulWidget {
 }
 
 class _ManagementSystemState extends State<ManagementSystem> {
-  List<Map<String, dynamic>> customers = [];
+  List<Map<String, dynamic>> customers = [
+    // Add your initial customer data here
+  ];
+
+  int currentPage = 0;
+  int rowsPerPage = 9; // Change this to the number of rows you want per page
 
   @override
   Widget build(BuildContext context) {
+    int start = currentPage * rowsPerPage;
+    int end = start + rowsPerPage;
+    List<Map<String, dynamic>> paginatedCustomers = customers.sublist(
+      start,
+      end > customers.length ? customers.length : end,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Customer Management'),
         actions: [
-          ElevatedButton(
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              side: BorderSide(color: Colors.blue),
+            ),
             onPressed: () => _showCustomerDialog(),
-            child: const Text('Create Customer'),
+            child: const Text(
+              'Create Customer',
+              style: TextStyle(color: Colors.blue),
+            ),
           ),
         ],
       ),
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: EdgeInsets.only(
+                left: 8, right: MediaQuery.of(context).size.width / 1.3),
             child: TextField(
               decoration: InputDecoration(
                 hintText: 'Search here....',
@@ -36,52 +55,134 @@ class _ManagementSystemState extends State<ManagementSystem> {
               ),
             ),
           ),
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: DataTable(
-                columns: const [
-                  DataColumn(label: Text('Customer Code')),
-                  DataColumn(label: Text('Customer Type')),
-                  DataColumn(label: Text('Company/Customer Name')),
-                  DataColumn(label: Text('Mobile Number')),
-                  DataColumn(label: Text('Credit Limit')),
-                  DataColumn(label: Text('Account Balance')),
-                  DataColumn(label: Text('Action')),
-                ],
-                rows: customers.asMap().entries.map((entry) {
-                  int index = entry.key;
-                  Map<String, dynamic> customer = entry.value;
-                  return DataRow(
-                    cells: [
-                      DataCell(Text(customer['code'])),
-                      DataCell(Text(customer['type'])),
-                      DataCell(Text(customer['name'])),
-                      DataCell(Text(customer['mobile'])),
-                      DataCell(Text(customer['creditLimit'].toString())),
-                      DataCell(Text(customer['accountBalance'].toString())),
-                      DataCell(Row(
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.edit),
-                            onPressed: () =>
-                                _showCustomerDialog(editIndex: index),
-                          ),
-                          IconButton(
-                              icon: const Icon(Icons.visibility),
-                              onPressed: () {}),
-                          IconButton(
-                              icon: const Icon(Icons.car_rental),
-                              onPressed: () {}),
-                        ],
-                      )),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  DataTable(
+                    headingRowColor:
+                        MaterialStateProperty.all<Color>(Colors.blue[50]!),
+                    border: const TableBorder.symmetric(),
+                    columns: const [
+                      DataColumn(label: Text('Customer Code')),
+                      DataColumn(label: Text('Customer Type')),
+                      DataColumn(label: Text('Company/Customer Name')),
+                      DataColumn(label: Text('Mobile Number')),
+                      DataColumn(label: Text('Credit Limit')),
+                      DataColumn(label: Text('Account Balance')),
+                      DataColumn(label: Text('Action')),
                     ],
-                  );
-                }).toList(),
+                    rows: paginatedCustomers.asMap().entries.map((entry) {
+                      int index = entry.key;
+                      Map<String, dynamic> customer = entry.value;
+                      return DataRow(
+                        color: MaterialStateProperty.resolveWith<Color?>(
+                          (Set<MaterialState> states) {
+                            return index.isEven
+                                ? Colors.white
+                                : Colors.blue[50];
+                          },
+                        ),
+                        cells: [
+                          DataCell(Text(customer['code'])),
+                          DataCell(Text(customer['type'])),
+                          DataCell(Text(customer['name'])),
+                          DataCell(Text(customer['mobile'])),
+                          DataCell(Text(customer['creditLimit'].toString())),
+                          DataCell(Text(customer['accountBalance'].toString())),
+                          DataCell(Row(
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.edit),
+                                onPressed: () =>
+                                    _showCustomerDialog(editIndex: index),
+                              ),
+                              IconButton(
+                                  icon: const Icon(Icons.visibility),
+                                  onPressed: () {}),
+                              IconButton(
+                                  icon: const Icon(Icons.car_rental),
+                                  onPressed: () {}),
+                            ],
+                          )),
+                        ],
+                      );
+                    }).toList(),
+                  ),
+                ],
               ),
             ),
           ),
-          // Pagination controls would go here
+          Padding(
+            padding: EdgeInsets.only(
+                left: MediaQuery.of(context).size.width / 4,
+                right: MediaQuery.of(context).size.width / 4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Showing ${start + 1} to ${end > customers.length ? customers.length : end} of ${customers.length} entries',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.chevron_left),
+                          onPressed: currentPage > 0
+                              ? () {
+                                  setState(() {
+                                    currentPage--;
+                                  });
+                                }
+                              : null,
+                        ),
+                        for (int i = 1;
+                            i <= (customers.length / rowsPerPage).ceil();
+                            i++)
+                          ElevatedButton(
+                            child: Text('$i'),
+                            style: ElevatedButton.styleFrom(
+                              shape: CircleBorder(),
+                              foregroundColor: currentPage == i - 1
+                                  ? Colors.white
+                                  : Colors.black,
+                              backgroundColor: currentPage == i - 1
+                                  ? Colors.blue
+                                  : Colors.grey[200],
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                currentPage = i - 1;
+                              });
+                            },
+                          ),
+                        IconButton(
+                          icon: Icon(Icons.chevron_right),
+                          onPressed:
+                              (currentPage + 1) * rowsPerPage < customers.length
+                                  ? () {
+                                      setState(() {
+                                        currentPage++;
+                                      });
+                                    }
+                                  : null,
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
